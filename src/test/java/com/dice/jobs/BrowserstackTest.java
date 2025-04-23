210a8f2bcb41e0ba9243b9f5d8ee9594229171aa
Prem Sagar <prems@MacBook-Pro.local>
2025-04-23 22:41:46 +0530

    updated workflow

--- src/test/java/com/dice/jobs/BrowserstackTest.java
+++ src/test/java/com/dice/jobs/BrowserstackTest.java
@@ -11,23 +11,27 @@ import java.net.URL;
 public class BrowserstackTest {
     protected WebDriver driver;
 
-    // ✅ Set your credentials here
     public static final String USERNAME = "premsagarmulugur_6PlhOI";
     public static final String AUTOMATE_KEY = "FLtzHkfbrEqZHDEK1DJm";
     public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
-    @Parameters({ "os", "os_version", "browser", "browser_version" })
     @BeforeMethod(alwaysRun = true)
-    public void setUp(String os, String os_version, String browser, String browser_version) throws MalformedURLException {
+    @Parameters({ "os", "os_version", "browser", "browser_version" })
+    public void setUp(
+            @Optional("Windows") String os,
+            @Optional("10") String os_version,
+            @Optional("Chrome") String browser,
+            @Optional("latest") String browser_version
+    ) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("os", os);
         caps.setCapability("os_version", os_version);
         caps.setCapability("browser", browser);
         caps.setCapability("browser_version", browser_version);
-        caps.setCapability("name", "Dice Job Apply Automation"); // Test name
-        caps.setCapability("build", "Dice Apply Build 01");       // CI build name
-        caps.setCapability("browserstack.debug", true);           // Enable visual logs
-        caps.setCapability("browserstack.networkLogs", true);     // Enable network logs
+        caps.setCapability("name", "Dice Job Apply Automation");
+        caps.setCapability("build", "Dice Apply Build 01");
+        caps.setCapability("browserstack.debug", true);
+        caps.setCapability("browserstack.networkLogs", true);
 
         driver = new RemoteWebDriver(new URL(URL), caps);
     }
